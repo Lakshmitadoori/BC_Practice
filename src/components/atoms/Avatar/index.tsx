9647de20f153f9ad7b04d5b4b8d121c4033957e3
vinayladi27 <vinay.ladi@zemosolabs.com>
2023-03-01 13:44:56 +0530

    avatar atom added

---
+++ src/components/atoms/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { Avatar,AvatarProps } from "@mui/material";
+import React from "react";
+
+const MuiAvatar = (props: AvatarProps) => {
+  return <Avatar {...props}></Avatar>;
+};
+export default MuiAvatar;
